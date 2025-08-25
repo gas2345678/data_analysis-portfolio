b7c9d339e7ef2a5b3bdba992b22f6f5c7f414919
gas2345678 <gtno@gmail.com>
2025-08-25 13:58:08 -0300

    new files

--- R/diamonds.R
+++ R/diamonds.R
@@ -1,14 +1,40 @@
+# Let's use the default diamonds dataset to perform an exploratory analysis
+
+# we import the necessary libraries
+
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
 
 
+# We import the 'diamonds' dataframe and read it like a csv file
+diamonds = read_csv(diamonds)
 
-
-data("diamonds",package="ggplot2")
-diamonds
-
-
-diamonds %>% select(carat,price)
-
-elite_diamonds <- diamonds %>% filter(price > 2000 & cut == "Premium")
-View(elite_diamonds)
+#Preview the dataframe
 View(diamonds)
+
+
+### Suppose you want to filter the diamonds with the max number of cuts.
+
+## We use the next command
+
+
+
+best_diamonds = diamonds %>% filter(cut == "Ideal")
+
+### Here we define a variable which filters the diamonds with the best cut.
+
+
+
+
+### Suppose we want to visualize all diamonds grouped by cut
+# Then we will use a barplot to visualize all quantities.
+
+
+bar_cut_diamonds = ggplot(data = diamonds,aes(x=cut)) + 
+  geom_bar() +
+  labs(title="Diamonds count grouped by cut")
+
+# Now, let's visualize it 
+View(bar_cut_diamonds)
 
