dae7da681525e0a9389a58b4ba94d7d428319162
gas2345678 <gtno@gmail.com>
2025-08-26 17:39:01 -0300

    New uber project

---
+++ R/uber_analytics.R
@@ -0,0 +1,23 @@
+
+#We import the necessary libraries
+
+ibrary(dplyr)
+library(ggplot2)
+
+
+
+#We import the uber dataframe
+
+uber_analytics = read.csv("C:/Users/PC/Downloads/uber_analytics.csv")
+
+
+
+
+
+
+
+
+
+
+
+
