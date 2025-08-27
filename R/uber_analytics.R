b9a6ba6cc16a9ecc7d467eeb4ab218868ed44199
gas2345678 <gtno@gmail.com>
2025-08-26 21:53:09 -0300

    new changes

--- R/uber_analytics.R
+++ R/uber_analytics.R
@@ -3,15 +3,25 @@
 
 library(dplyr)
 library(ggplot2)
+library(tidyr)
 
 
 
 #We import the uber dataframe
 
-
-
-uber_analytics = read.csv("C:/Users/PC/Downloads/uber_analytics.csv")
+uber_analytics <- read.csv("C:/Users/PC/Downloads/uber_analytics.csv")
 View(uber_analytics)
+
+# Drop null values for better reading
+
+
+uber_analytics %>% drop_na()
+uber_analytics
+
+
+
+
+
 
 #Worst rated uber driver
 
@@ -27,8 +37,9 @@ min_rating
 # Numbers of ubers vehicles grouped by vehicle type.
 
 
-vehicles_bar = ggplot(data=uber_analytics,aes(x=Vehicle.Type)) +
+ggplot(data=uber_analytics,aes(x=Vehicle.Type)) +
   geom_bar() + labs(title="Uber vehicles registered")
+
 
 
 
